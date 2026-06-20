72f29e35da455ed93ed1c2b573ed58b36540d129
Sujit Ajit Umbalkar <jeetumbalkar222@gmail.com>
2026-06-20 18:16:32 +0530

    Added Projection Interface to fetch required data from Entity , used for read only

--- hospitalManagementSystem/src/main/java/com/codingshuttle/springboot0To100/hospitalManagementSystem/repository/PatientRepository.java
+++ hospitalManagementSystem/src/main/java/com/codingshuttle/springboot0To100/hospitalManagementSystem/repository/PatientRepository.java
@@ -1,11 +1,17 @@
 package com.codingshuttle.springboot0To100.hospitalManagementSystem.repository;
 
+import com.codingshuttle.springboot0To100.hospitalManagementSystem.dto.IPatientInfo;
 import com.codingshuttle.springboot0To100.hospitalManagementSystem.entity.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PatientRepository extends JpaRepository<Patient, Long>
 {
+    @Query("select p.id as id , p.name as name , p.email as email from Patient p")
+    List<IPatientInfo> getAllPatientsInfo();
 
 }
--- hospitalManagementSystem/src/test/java/com/codingshuttle/springboot0To100/hospitalManagementSystem/PatientServiceTest.java
+++ hospitalManagementSystem/src/test/java/com/codingshuttle/springboot0To100/hospitalManagementSystem/PatientServiceTest.java
@@ -1,10 +1,14 @@
 package com.codingshuttle.springboot0To100.hospitalManagementSystem;
 
+import com.codingshuttle.springboot0To100.hospitalManagementSystem.dto.IPatientInfo;
+import com.codingshuttle.springboot0To100.hospitalManagementSystem.entity.Patient;
 import com.codingshuttle.springboot0To100.hospitalManagementSystem.repository.PatientRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -16,7 +20,30 @@ public class PatientServiceTest
     @Test
     public void testPatient()
     {
+        System.out.println("Printing All Data from Entity");
 
+        List<Patient> patients = patientRepository.findAll();
+        for(Patient patient : patients)
+        {
+            System.out.println(patient);
+        }
+
+        // / /////////////////////////////////////////
+        System.out.println("Printing Required Data using Projection Interface ");
+
+        List<IPatientInfo> patients1 = patientRepository.getAllPatientsInfo();
+
+    //        for(IPatientInfo patientInfo : patients1) {System.out.println(patientInfo);}
+    // Directly printing may show a proxy object instead of meaningful data,
+    // so we access values through getter methods.
+
+        for(IPatientInfo p : patients1)
+        {
+            System.out.printf("%d %s %s%n",
+                    p.getId(),
+                    p.getName(),
+                    p.getEmail());
+        }
     }
 
 }
