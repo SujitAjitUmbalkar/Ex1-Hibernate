2499044f851e17e6a9e1aa1913d741a78e6263ac
Sujit Ajit Umbalkar <jeetumbalkar222@gmail.com>
2026-06-20 21:34:08 +0530

    Entity LifeCycle Started ....

---
+++ hospitalManagementSystem/src/main/java/com/codingshuttle/springboot0To100/hospitalManagementSystem/service/PatientService.java
@@ -0,0 +1,24 @@
+package com.codingshuttle.springboot0To100.hospitalManagementSystem.service;
+
+import com.codingshuttle.springboot0To100.hospitalManagementSystem.entity.Patient;
+import com.codingshuttle.springboot0To100.hospitalManagementSystem.repository.PatientRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PatientService
+{
+    private final PatientRepository patientRepository;
+
+    public  void testPatientTransaction()
+    {
+        Patient p1 = patientRepository.findById(1L).orElse(null);
+        Patient p2 = patientRepository.findById(1L).orElse(null);         // tryng to find same used 2 times
+
+        System.out.println(p1) ;
+        System.out.println(p2);
+        System.out.println(p1==p2);
+        // without @@transactional it would differ
+    }
+}
