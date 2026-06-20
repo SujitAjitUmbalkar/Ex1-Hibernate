babaa6eff4746f373d2247aa45af27c9926f6b8c
Sujit Ajit Umbalkar <jeetumbalkar222@gmail.com>
2026-06-20 09:29:42 +0530

    Start Projection , prerequisits added , temp database added for tests

---
+++ hospitalManagementSystem/src/main/java/com/codingshuttle/springboot0To100/hospitalManagementSystem/entity/Patient.java
@@ -0,0 +1,33 @@
+package com.codingshuttle.springboot0To100.hospitalManagementSystem.entity;
+
+import com.codingshuttle.springboot0To100.hospitalManagementSystem.entity.type.BloodGroupType;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDate;
+
+@Entity
+@ToString
+@Getter
+@Setter
+public class Patient
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    private BloodGroupType bloodGroup;
+
+    @CreationTimestamp
+    private LocalDate birthDate;
+
+    private String email;
+
+    private String gender;
+}
