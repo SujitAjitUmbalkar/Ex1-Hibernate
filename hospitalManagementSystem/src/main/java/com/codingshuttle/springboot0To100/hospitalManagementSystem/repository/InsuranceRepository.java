a21226fdd6393f070aba4ceceb523e4dc3c48d66
Sujit Ajit Umbalkar <jeetumbalkar222@gmail.com>
2026-06-21 17:41:00 +0530

    Added repositories using JpaBuddy (plugin) , (steps - double shift -> search plugins -> JPABuddy -> install and apply )

---
+++ hospitalManagementSystem/src/main/java/com/codingshuttle/springboot0To100/hospitalManagementSystem/repository/InsuranceRepository.java
@@ -0,0 +1,8 @@
+package com.codingshuttle.springboot0To100.hospitalManagementSystem.repository;
+
+import com.codingshuttle.springboot0To100.hospitalManagementSystem.entity.Insurance;
+import org.springframework.data.repository.Repository;
+
+public interface InsuranceRepository extends Repository<Insurance, Long>
+{
+}
